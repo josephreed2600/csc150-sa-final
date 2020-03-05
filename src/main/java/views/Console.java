2ea2c85606c81ebbd887a2f8a2e325f8a3df05a3
Joseph Reed <josephreed2600@gmail.com>
2020-03-05 09:07:32 -0700

    realize most of IView in Console

--- src/main/java/views/Console.java
+++ src/main/java/views/Console.java
@@ -1,6 +1,9 @@
 package views;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.*;
+import java.util.function.Function;
 
 public class Console implements IView {
 
@@ -14,11 +17,7 @@ public class Console implements IView {
 		cout.print(s);
 	}
 
-	public String read() {
-		return prompt("");
-	}
-
-	public String prompt(String prompt) {
+	public String promptForString(String prompt) {
 		IOException ioe = null;
 		do {
 			try {
@@ -32,15 +31,11 @@ public class Console implements IView {
 		throw new RuntimeException(this.getClass().getSimpleName()+"::"+(new Object().getClass().getEnclosingMethod().getName())+" failed successfully");
 	}
 
-	public int readInt() {
-		return promptForInt("");
-	}
-
 	public int promptForInt(String prompt) {
 		NumberFormatException nfe = null;
 		do {
 			try {
-				String s = prompt(prompt);
+				String s = promptForString(prompt);
 				return Integer.parseInt(s);
 			} catch(NumberFormatException e) {
 				nfe = e;
@@ -48,5 +43,78 @@ public class Console implements IView {
 			}
 		} while(nfe != null);
 		throw new RuntimeException(this.getClass().getSimpleName()+"::"+(new Object().getClass().getEnclosingMethod().getName())+" failed successfully");
+	}
+
+	public boolean promptForBoolean(String prompt) {
+		RuntimeException re = null;
+		do {
+			try {
+				String s = promptForString(prompt);
+				switch(s.toLowerCase()) {
+					case "t":
+					case "true":
+					case "y":
+					case "yes":
+					case "1":
+						return true;
+					case "f":
+					case "false":
+					case "n":
+					case "no":
+					case "0":
+						return false;
+					default:
+						throw new RuntimeException("Unknown value");
+				}
+			} catch(RuntimeException e) {
+				re = e;
+				display("Not a boolean value\n");
+			}
+		} while(re != null);
+		throw new RuntimeException(this.getClass().getSimpleName()+"::"+(new Object().getClass().getEnclosingMethod().getName())+" failed successfully");
+	}
+
+	public ArrayList<Integer> promptForInts(String prompt) {
+		Pattern regex = Pattern.compile("(?:.*([0-9]+).*)");
+		String s = promptForString(prompt);
+		Matcher m = regex.matcher(s);
+		/*
+		if(m.find()) {
+		}
+		*/
+		//return new ArrayList<Integer>();
+		throw new UnsupportedOperationException("Unimplemented");
+	}
+
+	public String promptForString(String prompt, Function<String, String> validator) {
+		String input, error;
+		do {
+			input = promptForString(prompt);
+			error = validator.apply(input);
+			if(error != null) display(error + '\n');
+		} while(error != null);
+		return input;
+	}
+
+	public int promptForInt(String prompt, Function<Integer, String> validator) {
+		Integer input;
+		String error;
+		do {
+			input = promptForInt(prompt);
+			error = validator.apply(input);
+			if(error != null) display(error + '\n');
+		} while(error != null);
+		return input;
+	}
+
+	public ArrayList<Integer> promptForInts(String prompt, Function<ArrayList<Integer>, String> validator) {
+		ArrayList<Integer> input;
+		String error;
+		do {
+			input = promptForInts(prompt);
+			error = validator.apply(input);
+			if(error != null) display(error + '\n');
+		} while(error != null);
+		return input;
 	}
 }

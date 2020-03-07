44eb2a20d11bac12d4cd1937e749d66c878c4f55
Joseph Reed <josephreed2600@gmail.com>
2020-03-07 16:44:35 -0700

    add toString to RuleList

--- src/main/java/gameoflife/models/RuleList.java
+++ src/main/java/gameoflife/models/RuleList.java
@@ -12,4 +12,23 @@ public class RuleList extends ArrayList<IRule> implements IRule {
 		}
 		return workingCell;
 	}
+
+	/**
+	 * Returns a string containing the stringified forms of the contained
+	 * rules, separated by the ASCII unit separator (31), terminated by
+	 * the ASCII record separator (30).
+	 */
+	@Override
+	public String toString() {
+		StringBuilder out = new StringBuilder();
+		String sep = "";
+		String unitSep = Character.toString((char)31);
+		String recordSep = Character.toString((char)30);
+		for(IRule rule : this) {
+			out.append(sep).append(rule);
+			sep = unitSep;
+		}
+		out.append(recordSep);
+		return out.toString();
+	}
 }

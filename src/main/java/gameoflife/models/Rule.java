87adb63edbae792d50481ac0ed601705d56bf05a
Joseph Reed <josephreed2600@gmail.com>
2020-03-07 15:58:43 -0700

    add Rule

---
+++ src/main/java/gameoflife/models/Rule.java
@@ -0,0 +1,20 @@
+package gameoflife.models;
+
+import java.util.function.BiFunction;
+
+import gameoflife.models.IRule;
+
+public class Rule implements IRule {
+	private final String description;
+	private final BiFunction<CellState, CellState[], CellState> rule;
+
+	public Rule(String description, BiFunction<CellState, CellState[], CellState> rule) {
+		this.description = description;
+		this.rule = rule;
+	}
+
+	@Override
+	public CellState apply(CellState cell, CellState[] neighbors) {
+		return rule.apply(cell, neighbors);
+	}
+}

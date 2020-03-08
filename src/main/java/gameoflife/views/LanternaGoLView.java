b02b602b6448e2801dd65d162561cb9cf4dc7875
Joseph Reed <josephreed2600@gmail.com>
2020-03-07 17:29:03 -0700

    initialize LanternaGoLView

---
+++ src/main/java/gameoflife/views/LanternaGoLView.java
@@ -0,0 +1,35 @@
+package gameoflife.views;
+
+import gameoflife.models.BoardState;
+import gameoflife.models.CellStateList;
+import gameoflife.models.RuleList;
+
+import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.screen.*;
+import com.googlecode.lanterna.gui2.*;
+
+public class LanternaGoLView implements IGoLView{
+	
+	private Terminal term;
+	private Screen screen;
+	private WindowBasedTextGUI tui;
+
+	public void displayBoard(BoardState boardState) {
+	}
+
+	public void modifyBoard(BoardState boardState) {
+	}
+
+	public void displayCellStates(CellStateList cellStates) {
+	}
+
+	public void modifyCellStates(CellStateList cellStates) {
+	}
+
+	public void displayRules(RuleList rules) {
+	}
+
+	public void modifyRules(RuleList rules) {
+	}
+
+}

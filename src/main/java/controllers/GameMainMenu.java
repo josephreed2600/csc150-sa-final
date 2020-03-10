70018a7bdcae6a8479643e0acbaade7ab76afe9b
Joseph Reed <josephreed2600@gmail.com>
2020-03-10 09:46:10 -0600

    clean up main menu

--- src/main/java/controllers/GameMainMenu.java
+++ src/main/java/controllers/GameMainMenu.java
@@ -16,32 +16,24 @@ public class GameMainMenu implements IController {
     this.view = view;
   }
 
-  private String introText;
-  // do we need this?
-  private String encryptionString = "";
-
   public void run() {
-    introText = "Welcome To Conway's Game Of Life/Go Fish!";
-    mainMenu();
-  }
-
-  public int mainMenu() {
     int userChoice = -2;
 
     do {
-      StringBuilder mainMenu = new StringBuilder(introText);
-
-      mainMenu.append("Choose A Option Below: \n");
-      mainMenu.append("1)Play Conway's Game Of Life\n");
-      mainMenu.append("2)Play Go Fish VS A Computer\n");
-      mainMenu.append("0)Exit The Application\n\n");
-      mainMenu.append("Enter You Choice: ");
-      userChoice = promptForInt(mainMenu.toString(), 0, 2);
+      StringBuilder mainMenu = new StringBuilder('\n')
+			.append("Welcome To Conway's Game of Life & Go Fish!\n")
+      .append("Choose an option: \n")
+      .append("  1) Play Conway's Game Of Life\n")
+      .append("  2) Play Go Fish VS A Computer\n\n")
+      .append("  0) Exit The Application\n\n")
+      .append("Enter your selection [0-2]: ");
+      userChoice = view.promptForInt(mainMenu.toString(), (input) -> {if(0 <= input && input <= 2) return null; return "Out of bounds; please try again [0-2]";});
 
       switch (userChoice) {
         case 0:
-          System.out.println("Goodbye!");
+          System.out.println("\nGoodbye!");
           break;
+
         case 1:
           // Game of Life goes here
           new GameOfLife(new LanternaGoLView()).run();
@@ -53,65 +45,11 @@ public class GameMainMenu implements IController {
           break;
 
         default:
-          System.out.println("Not A Valid Choice");
+          System.out.println("Not a valid choice; please try again [0-2]");
           break;
       }
 
     } while (userChoice != 0);
-
-    return userChoice;
   }
 
-  public String promptForString(String prompt) {
-    // declare buffered reader
-    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    // declare and initialize to null string input
-    String input = null;
-    // Declare and initialize to true boolean isInvalid
-    boolean isInvalid = true;
-
-    do {
-      System.out.println(prompt);
-      // try
-      try {
-        input = bf.readLine();
-        // set isInvalid to try if input is null, empty or just white space
-        isInvalid = input == null || input.trim().isEmpty();
-        // if isInvalid is true
-        if (isInvalid) {
-          // inform the user their input was invalid
-          System.out.println(
-              "Your input cannot be null, empty, or just white space, pls try again");
-        }
-      } catch (IOException ioe) {
-        System.out.println("Tech issue pls try again");
-      }
-
-    } while (isInvalid);
-    encryptionString = input;
-    return input;
-  }
-
-  public int promptForInt(String prompt, int min, int max) {
-    int userNum = 0;
-
-    // declare and initialize to true boolean isInvalid
-    boolean isInvalid = true;
-    // declare String input and initialize to promptForString(prompt)
-    do {
-      String input = promptForString(prompt);
-
-      try {
-        userNum = Integer.parseInt(input);
-        isInvalid = userNum < min || userNum > max;
-      } catch (NumberFormatException nfe) {
-        System.out.println("enter a valid number");
-      }
-      if (isInvalid == true) {
-        System.out.println("Please enter a valid number");
-      }
-
-    } while (isInvalid);
-    return userNum;
-  }
 }

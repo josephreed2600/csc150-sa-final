747f8f3ef7d64c460b2144da2d4269655120fc74
Joseph Reed <josephreed2600@gmail.com>
2020-03-11 00:56:24 -0600

    organize default pattern for GoL

--- src/main/java/gameoflife/controllers/GameOfLife.java
+++ src/main/java/gameoflife/controllers/GameOfLife.java
@@ -62,36 +62,6 @@ public class GameOfLife {
               System.out.print("]\n");
               return cell;
             });
-    /*
-    	 rules.add(new Rule("birth", (cell, neighbors) -> {
-    // if it's not dead, we don't care
-    if(!cell.equals(cellStates.findByName("dead"))) return cell;
-    int livingNeighbors = Rule.countState(neighbors, cellStates.findByName("alive"));
-    // if it has three live neighbors, be alive
-    if(livingNeighbors == 3) return cellStates.findByName("alive");
-    // otherwise, no change
-    return cell;
-    }));
-    rules.add(new Rule("overpopulation", (cell, neighbors) -> {
-    // if it's not alive, we don't care
-    if(!cell.equals(cellStates.findByName("alive"))) return cell;
-    int livingNeighbors = Rule.countState(neighbors, cellStates.findByName("alive"));
-    // if it has more than three live neighbors, then perish
-    if(livingNeighbors > 3) return cellStates.findByName("dead");
-    // otherwise, no change
-    return cell;
-    }));
-    rules.add(new Rule("loneliness", (cell, neighbors) -> {
-    // if it's not alive, we don't care
-    if(!cell.equals(cellStates.findByName("alive"))) return cell;
-    int livingNeighbors = Rule.countState(neighbors, cellStates.findByName("alive"));
-    // if it has more than three live neighbors, then perish
-    if(livingNeighbors < 2) return cellStates.findByName("dead");
-    // otherwise, no change
-    return cell;
-    }));
-     */
-    /**/
     Rule classicRule =
         new Rule(
             "classic",
@@ -132,27 +102,35 @@ public class GameOfLife {
   }
 
   public void applyTestPattern() {
-    /**/
-    currentState.setCell(0, 0, cellStates.findByName("dead"));
-    currentState.setCell(0, 1, cellStates.findByName("alive"));
-    currentState.setCell(0, 2, cellStates.findByName("dead"));
-    currentState.setCell(1, 0, cellStates.findByName("dead"));
-    currentState.setCell(1, 1, cellStates.findByName("dead"));
-    currentState.setCell(1, 2, cellStates.findByName("alive"));
-    currentState.setCell(2, 0, cellStates.findByName("alive"));
-    currentState.setCell(2, 1, cellStates.findByName("alive"));
-    currentState.setCell(2, 2, cellStates.findByName("alive"));
-    /*/
-    	currentState.setCell(0, 0, cellStates.findByName("dead"));
-    	currentState.setCell(0, 1, cellStates.findByName("dead"));
-    	currentState.setCell(0, 2, cellStates.findByName("dead"));
-    	currentState.setCell(1, 0, cellStates.findByName("alive"));
-    	currentState.setCell(1, 1, cellStates.findByName("alive"));
-    	currentState.setCell(1, 2, cellStates.findByName("alive"));
-    	currentState.setCell(2, 0, cellStates.findByName("dead"));
-    	currentState.setCell(2, 1, cellStates.findByName("dead"));
-    	currentState.setCell(2, 2, cellStates.findByName("dead"));
-    /**/
+    String initial = "glider";
+    switch (initial) {
+      case "glider":
+        // Glider bearing SE
+        currentState.setCell(0, 0, cellStates.findByName("dead"));
+        currentState.setCell(0, 1, cellStates.findByName("alive"));
+        currentState.setCell(0, 2, cellStates.findByName("dead"));
+        currentState.setCell(1, 0, cellStates.findByName("dead"));
+        currentState.setCell(1, 1, cellStates.findByName("dead"));
+        currentState.setCell(1, 2, cellStates.findByName("alive"));
+        currentState.setCell(2, 0, cellStates.findByName("alive"));
+        currentState.setCell(2, 1, cellStates.findByName("alive"));
+        currentState.setCell(2, 2, cellStates.findByName("alive"));
+        break;
+      case "log":
+        // 1x3
+        currentState.setCell(0, 0, cellStates.findByName("dead"));
+        currentState.setCell(0, 1, cellStates.findByName("dead"));
+        currentState.setCell(0, 2, cellStates.findByName("dead"));
+        currentState.setCell(1, 0, cellStates.findByName("alive"));
+        currentState.setCell(1, 1, cellStates.findByName("alive"));
+        currentState.setCell(1, 2, cellStates.findByName("alive"));
+        currentState.setCell(2, 0, cellStates.findByName("dead"));
+        currentState.setCell(2, 1, cellStates.findByName("dead"));
+        currentState.setCell(2, 2, cellStates.findByName("dead"));
+        break;
+      default:
+        break;
+    }
   }
 
   public void run() {
